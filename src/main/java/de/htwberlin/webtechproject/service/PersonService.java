eff90757a4dbee372668613063be47d4b09dff00
lennartmanske <lennartmanske@gmail.com>
2022-11-15 20:32:29 +0100

    M2 progress

---
+++ src/main/java/de/htwberlin/webtechproject/service/PersonService.java
@@ -0,0 +1,42 @@
+package de.htwberlin.webtechproject.service;
+
+import de.htwberlin.webtechproject.persistence.PersonEntity;
+import de.htwberlin.webtechproject.persistence.PersonRepository;
+import de.htwberlin.webtechproject.web.api.Person;
+import de.htwberlin.webtechproject.web.api.PersonCreateRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll() {
+        List<PersonEntity> persons = personRepository.findAll();
+        return persons.stream()
+                .map(this::transformEntity).collect(Collectors.toList());
+    }
+
+    public Person create(PersonCreateRequest request) {
+        var personEntity = new PersonEntity(request.getFirstName(), request.getLastName(), request.isVaccinated());
+        personEntity = personRepository.save(personEntity);
+        return transformEntity(personEntity);
+    }
+
+    private Person transformEntity(PersonEntity personEntity){
+        return new Person(
+                personEntity.getId(),
+                personEntity.getFirstName(),
+                personEntity.getLastName(),
+                personEntity.isVaccinated()
+        );
+    }
+
+}
--- src/main/java/de/htwberlin/webtechproject/web/api/PersonCreateRequest.java
+++ src/main/java/de/htwberlin/webtechproject/web/api/PersonCreateRequest.java
@@ -1,38 +1,16 @@
-package de.htwberlin.webtechproject.persistence;
+package de.htwberlin.webtechproject.web.api;
 
-import javax.persistence.*;
+public class PersonCreateRequest {
 
-@Entity(name = "persons")
-public class PersonEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private long id;
-
-    @Column(name = "first_name", nullable = false)
     private String firstName;
-
-    @Column(name = "last_name", nullable = false)
     private String lastName;
-
-    @Column(name = "is_vaccinated")
     private boolean vaccinated;
 
-
-    public PersonEntity(String firstName, String lastName, boolean vaccinated) {
-
+    public PersonCreateRequest(String firstName, String lastName, boolean vaccinated) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.vaccinated = vaccinated;
     }
-
-    protected PersonEntity() {}
-
-    public long getId() {
-        return id;
-    }
-
 
     public String getFirstName() {
         return firstName;
